767d32c4dde26b408963f487939bd7c918fd110a
Abel Gazmuri <abel.gazmuri.m@gmail.com>
2022-03-15 18:11:07 -0300

    Se crea componente de ingreso de clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngresoClientesComponent } from './ingreso-clientes/ingreso-clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresoClientesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ingreso-clientes/ingreso-clientes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-clientes',
+  templateUrl: './ingreso-clientes.component.html',
+  styleUrls: ['./ingreso-clientes.component.css']
+})
+export class IngresoClientesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
