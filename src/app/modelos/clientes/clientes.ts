092dba0b2e6aab206dccd4216fec3f8273d02ea1
Abel Gazmuri <abel.gazmuri.m@gmail.com>
2022-03-15 18:05:06 -0300

    Inicio proyecto codefly

---
+++ src/app/modelos/clientes/clientes.ts
@@ -0,0 +1,13 @@
+export class Clientes{
+
+
+    constructor(id_cliente:number,
+                rut : string,
+                nombre : string,
+                apellido:string,
+                correo : string,
+                movil : string,
+                telefono: string
+        ){}
+
+}
